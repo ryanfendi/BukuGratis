3d8027b7473fc15b86b6e6582e605784089b329e
ryanfendi <102475046+ryanfendi@users.noreply.github.com>
2026-08-01 11:35:42 +0700

    Refactor server.js to simplify app initialization

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,9 @@
-import express from "express";
-import cors from "cors";
+import app from "./app.js";
 
-import books from "./routes/books.js";
-import upload from "./routes/upload.js";
-import auth from "./routes/auth.js";
+const PORT = process.env.PORT || 3000;
 
-const app = express();
+app.listen(PORT, () => {
 
-app.use(cors());
-
-app.use(express.json());
-
-app.use("/books", books);
-
-app.use("/upload", upload);
-
-app.use("/auth", auth);
-
-app.get("/", (req,res)=>{
-
-res.json({
-
-app:"BukuGratis.com",
-
-version:"1.0",
-
-status:"Online"
-
-});
-
-});
-
-const PORT=process.env.PORT||3000;
-
-app.listen(PORT,()=>{
-
-console.log("Server Running");
+    console.log("Server Running Port", PORT);
 
 });
